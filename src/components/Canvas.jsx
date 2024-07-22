8052e5cc925c546a96fc0924e29ce3206618789e
HanunagreZ <hanunagrez@yandex.ru>
2024-07-22 09:39:07 +0300

    feat: add and configure tools: brush, circle, rect, eraser, line

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,11 +1,25 @@
+import { useEffect } from 'react';
+import { observer } from 'mobx-react-lite';
+import { useRef } from 'react';
 import '../styles/canvas.scss';
+import canvasState from '../store/canvasState';
+import toolState from '../store/toolState';
+import Brush from '../tools/Brush';
 
-const Canvas = () => {
+const Canvas = observer(() => {
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    console.log(canvasRef.current);
+    canvasState.setCanvas(canvasRef.current);
+    toolState.setTool(new Brush(canvasRef.current));
+  }, []);
+
   return (
     <div className="canvas">
-      <canvas width={600} height={400} />
+      <canvas ref={canvasRef} width={600} height={400} />
     </div>
   );
-};
+});
 
 export default Canvas;
